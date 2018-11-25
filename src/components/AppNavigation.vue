e55b59c109f790989a9c34a16aeae924f3b2e768
Carlos Azaustre <cazaustre@gmail.com>
2018-11-25 07:20:13 +0100

    App Navigation

---
+++ src/components/AppNavigation.vue
@@ -0,0 +1,51 @@
+<template>
+  <span>
+    <v-navigation-drawer 
+      class="brown-lighten-2" 
+      app
+      v-model="drawer" 
+      dark 
+      disable-resizer-watcher>
+      <v-list>
+        <template v-for="(item, index) in items">
+          <v-list-tile :key="index">
+            <v-list-tile-content>
+              {{ item.title }}
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider :key="`divider-${index}`"></v-divider>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-toolbar app color="brown darken-4" dark>
+      <v-toolbar-side-icon 
+        class="hidden-md-and-up"
+        @click="drawer = !drawer">
+      </v-toolbar-side-icon>
+      <v-spacer class="hidden-md-and-up"></v-spacer>
+      <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+      <v-btn class="hidden-sm-and-down" flat>Menu</v-btn>
+      <v-spacer class="hidden-sm-and-down"></v-spacer>
+      <v-btn class="hidden-sm-and-down" flat>SIGN IN</v-btn>
+      <v-btn class="hidden-sm-and-down" color="brown lighten-3">JOIN</v-btn>
+    </v-toolbar>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "AppNavigation",
+  data () {
+    return {
+      appTitle: "Meal Prep",
+      drawer: false,
+      items: [
+        { title: "Menu" },
+        { title: "Sign In" },
+        { title: "Join" }
+      ]
+    }
+  }
+};
+</script>
