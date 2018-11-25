28e81d172e63958bb96693992ed39a6b92b55cce
Carlos Azaustre <cazaustre@gmail.com>
2018-11-25 08:24:10 +0100

    HomeDetails

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div></div>
+  <span>
+    <home-hero></home-hero>
+    <home-details></home-details>
+  </span>
 </template>
 
 <script>
+import HomeHero from "@/components/HomeHero";
+import HomeDetails from "@/components/HomeDetails";
+
 export default {
-  name: "home"
+  name: "home",
+  components: { HomeHero, HomeDetails }
 };
 </script>
