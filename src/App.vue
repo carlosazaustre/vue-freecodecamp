8e0eae058eab83f46acdc38d47079999cdda8ae0
Carlos Azaustre <cazaustre@gmail.com>
2018-11-25 07:22:42 +0100

    lint fix

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import AppNavigation from '@/components/AppNavigation';
+import AppNavigation from "@/components/AppNavigation";
 
 export default {
   name: "App",
--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,17 +1,16 @@
 <template>
   <span>
-    <v-navigation-drawer 
-      class="brown-lighten-2" 
+    <v-navigation-drawer
+      class="brown-lighten-2"
       app
-      v-model="drawer" 
-      dark 
-      disable-resizer-watcher>
+      v-model="drawer"
+      dark
+      disable-resizer-watcher
+    >
       <v-list>
         <template v-for="(item, index) in items">
           <v-list-tile :key="index">
-            <v-list-tile-content>
-              {{ item.title }}
-            </v-list-tile-content>
+            <v-list-tile-content> {{ item.title }} </v-list-tile-content>
           </v-list-tile>
           <v-divider :key="`divider-${index}`"></v-divider>
         </template>
@@ -19,9 +18,7 @@
     </v-navigation-drawer>
 
     <v-toolbar app color="brown darken-4" dark>
-      <v-toolbar-side-icon 
-        class="hidden-md-and-up"
-        @click="drawer = !drawer">
+      <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer;">
       </v-toolbar-side-icon>
       <v-spacer class="hidden-md-and-up"></v-spacer>
       <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
@@ -36,16 +33,12 @@
 <script>
 export default {
   name: "AppNavigation",
-  data () {
+  data() {
     return {
       appTitle: "Meal Prep",
       drawer: false,
-      items: [
-        { title: "Menu" },
-        { title: "Sign In" },
-        { title: "Join" }
-      ]
-    }
+      items: [{ title: "Menu" }, { title: "Sign In" }, { title: "Join" }]
+    };
   }
 };
 </script>
