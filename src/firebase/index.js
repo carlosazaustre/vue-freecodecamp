10f77832757fc42288a7c153fa7fe88feacef125
Carlos Azaustre <cazaustre@gmail.com>
2018-11-26 08:49:42 +0100

    Join component template

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDd0NZkDVD0xmrfzAzB1JByCyl_KVzVcKE",
+  authDomain: "vue-freecodecamp.firebaseapp.com",
+  databaseURL: "https://vue-freecodecamp.firebaseio.com",
+  projectId: "vue-freecodecamp",
+  storageBucket: "vue-freecodecamp.appspot.com",
+  messagingSenderId: "914023537893"
+};
+
+firebase.initializeApp(config);
--- src/views/Join.vue
+++ src/views/Join.vue
@@ -1,14 +1,38 @@
 <template>
-  <v-container fluid>
-    <v-layout>
-      <h1>Join Page</h1>
+  <v-container fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+          <v-toolbar dark color="primary">
+            <v-toolbar-title>Join Form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form ref="form" v-model="valid" lazy-validation>
+              <v-text-field prepend-icon="person" name="email" label="Email" type="email" v-model="email" :rules="emailRules" required></v-text-field>
+              <v-text-field prepend-icon="lock" name="password" id="password" label="Password" type="password" v-model="password" :rules="passwordRules" required></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" :disabled="!valid" @click="submit">Join</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: "Join"
+  name: "Join",
+  data() {
+    return {
+      email: "",
+      pasword: "",
+      emailRules: "",
+      passwordRules: ""
+    };
+  }
 };
 </script>
 
