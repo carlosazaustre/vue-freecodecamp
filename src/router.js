cd5a12066b3caa4ad2b3c96cf0566c44bd31300b
Carlos Azaustre <cazaustre@gmail.com>
2018-11-28 09:51:20 +0100

    add routes guard

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import store from "./store";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
@@ -31,11 +32,24 @@ export default new Router({
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      component: () => import("./views/About.vue"),
+      meta: {
+        authRequired: true
+      }
     }
   ]
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.authRequired)) {
+    if (!store.state.user) {
+      next({ path: "/sign-in"});
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
